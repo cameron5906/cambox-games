0f4e8a6d51af9d5f7bbb1924789299ca23d83af8
Cameron Cooper <cameron@cameron.pw>
2021-03-15 05:04:49 -0400

    Fallback to PORT env (Heroku)

--- cambox-api/src/main.ts
+++ cambox-api/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen( process.env.CAMBOX_API_PORT );
+  await app.listen( process.env.CAMBOX_API_PORT || process.env.PORT );
 }
 bootstrap();
